ee0675f73ef711c516772c01a03dfd4c4ac7e673
Asin-Junior-Honore <asinhonore823@gmail.com>
2024-06-01 22:41:01 +0100

    changed time of scrape from 3 to 4

--- src/navigation.ts
+++ src/navigation.ts
@@ -1,5 +1,5 @@
+//use this if only you want pupetter to do navigation it self alll the way from homepage to the startup page
 import { Page } from 'puppeteer';
-
 export async function navigateToStartupsPage(page: Page) {
     const startupsLinkSelector = 'a.wp-block-navigation-item__content[href="/category/startups/"]';
     await page.waitForSelector(startupsLinkSelector);
