a9c9575068a7e4a61be9284bed3fdd3c466eb008
Asin-Junior-Honore <135389589+Asin-Junior-Honore@users.noreply.github.com>
2024-10-02 21:51:59 +0100

    Update scraping.ts

--- src/scraping.ts
+++ src/scraping.ts
@@ -2,16 +2,22 @@ import { Page } from 'puppeteer';
 
 export async function scrapeData(page: Page) {
     return await page.evaluate(() => {
-        const items = document.querySelectorAll('.wp-block-tc23-post-picker');
+        const items = document.querySelectorAll('li.wp-block-post');
         const results: { title: string, link: string, timeAgo: string }[] = [];
 
+        // Iterate through each item
         items.forEach(item => {
-            const titleElement = item.querySelector('h2') as HTMLElement;
-            const linkElement = titleElement?.querySelector('a') as HTMLAnchorElement;
-            const timeAgoElement = item.querySelector('.wp-block-tc23-post-time-ago') as HTMLElement;
+           
+            const titleElement = item.querySelector('.loop-card__title-link') as HTMLAnchorElement;
+            const timeAgoElement = item.querySelector('.loop-card__meta .wp-block-tc23-post-time-ago') as HTMLElement;
+
+           
             const title = titleElement?.innerText ?? 'Title not found';
-            const link = linkElement?.href ?? '#';
+            const link = titleElement?.href ?? '#';
             const timeAgo = timeAgoElement?.innerText ?? 'Time not found';
+
+        
+            // Push results
             results.push({ title, link, timeAgo });
         });
 
